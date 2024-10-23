134f6d8f1d32ac8c08eb4d479fe178c1c68d0d02
kamczill <kam134@interia.eu>
2024-10-23 08:00:19 +0200

    feat: update domains

--- app/[lang]/sections/FeaturedProjects.jsx
+++ app/[lang]/sections/FeaturedProjects.jsx
@@ -3,33 +3,34 @@ import image from "../../../public/project1.webp";
 import Headline from "@/app/components/Headline";
 import { fetchFromStrapi } from "../../utils/fetchFromStrapi";
 
-const FeaturedProjects = async ({dictionary}) => {
-  const res = await fetchFromStrapi('featured-projects1', dictionary.lang)
+const FeaturedProjects = async ({ dictionary }) => {
+  const res = await fetchFromStrapi("featured-projects1", dictionary.lang);
 
-  if(!res.ok){
-    return <p className="text-xl text-red-500 text-center">{dictionary.errors.error404}</p>
+  if (!res.ok) {
+    console.log(res);
+    return (
+      <p className="text-xl text-red-500 text-center">
+        {dictionary.errors.error404}
+      </p>
+    );
   }
-  
-  const json = await res.json()
-  const data = await json.data
 
-
+  const json = await res.json();
+  const data = await json.data;
 
   return (
     <section className="p-[10px] sm:p-[20px] lg:px-[60px] pt-5 border-t border-secondary flex flex-col gap-5">
       <Headline>{dictionary.homepage.featuredProjects.headline}</Headline>
-      
-      <div className="flex flex-col">
 
-        {data && (
-          data.map(project => (
+      <div className="flex flex-col">
+        {data &&
+          data.map((project) => (
             <FeaturedProject
-            data={project}
-            key={project.id}
-            dictionary={dictionary}
-          />
-          ))
-        )}
+              data={project}
+              key={project.id}
+              dictionary={dictionary}
+            />
+          ))}
 
         {/* <FeaturedProject
           year="2023"
